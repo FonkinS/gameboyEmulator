b6b1673fcbbce0babd31f61c7c82d53d5f822849
Fonkin Stubbleduck <fonkinstubbleduck@gmail.com>
2024-10-02 18:56:39 +0200

    No. 10 (surprisingly) worked out of the box

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main() {
     is_interrupting = false;
     IME = false;
 
-    FILE *file = fopen("demoFiles/blarggsTest/09-op r,r.gb", "rb");
+    FILE *file = fopen("demoFiles/blarggsTest/10-bit ops.gb", "rb");
     if (file == NULL) {
         printf("File Not Found!\n");
         return -1;
@@ -114,9 +114,9 @@ int main() {
             cycles += cycle_length;
             f &= 0xf0;
             
-            if (PC == 0xCB91) {
+            /*if (PC == 0xCB91) {
                 break;
-            }
+            }*/
         } else if (interrupt_next_op) {
             if (IME) {
                 just_interrupted = true;
