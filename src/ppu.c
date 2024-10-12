8d51a93e7cd4a778dcafe7aa9efdac3623c0d9a6
Fonkin Stubbleduck <fonkinstubbleduck@gmail.com>
2024-10-12 19:18:40 +0200

    Fixed bug with scrolling (turned out to be single misplaced bracked)

--- src/ppu.c
+++ src/ppu.c
@@ -7,6 +7,8 @@ SGFwindow window;
 GLubyte background_texture[160*144*3];
 SGFsprite bg;
 
+uint8_t colors[4][4] = {{155, 188, 15}, {139, 172, 15}, {48, 98, 48}, {15, 56, 15}};
+
 
 int init_ppu() {
     window = SGFCreateWindow(320,288, "Gameboy", 0);
@@ -29,6 +31,7 @@ void draw_bg() {
     int bg_td_start = (data[0xff40] & 16) ? 0x8000 : 0x9000;
     int scrollx = data[0xff43];
     int scrolly = data[0xff42];
+    //printf("%i, %i\n", scrollx, scrolly);
     int tilescrollx = scrollx / 8;
     int tilescrolly = scrolly / 8;
     // Note for future self, single row and column of edge tiles might not work
@@ -48,9 +51,14 @@ void draw_bg() {
                     for (int tx = 0; tx < 8; tx++) {
                         if (bg_x * 8 + tx-scrollx%8 < 0) continue;
                         uint8_t color = (((first >> (7-tx)) & 1) << 1) + ((second >> (7-tx)) & 1);
-                        background_texture[(bg_y*160*8 + (ty-scrolly%8)*160 + bg_x*8 + (tx-scrollx%8)) * 3] = 255 - color * 55;
-                        background_texture[(bg_y*160*8 + (ty-scrolly%8)*160 + bg_x*8 + (tx-scrollx%8)) * 3+1] = 255 - color * 55;
-                        background_texture[(bg_y*160*8 + (ty-scrolly%8)*160 + bg_x*8 + (tx-scrollx%8)) * 3+2] = 255 - color * 55;
+                        //uint16_t index = (bg_y*160*8 + (ty-scrolly%8)*160 + bg_x*8 + (tx-scrollx%8)) * 3;
+                        int index = ((bg_y * 8 + (ty-(scrolly%8))) * 160 + bg_x*8+(tx-scrollx%8)) * 3;
+                        background_texture[index] = colors[color][0];
+                        background_texture[index+1] = colors[color][1];
+                        background_texture[index+2] = colors[color][2];
+                        //background_texture[index] = 255;
+                        //background_texture[index+1] = 255;
+                        //background_texture[index+2] = 255;
                     }
                 }
             }
@@ -60,7 +68,7 @@ void draw_bg() {
 
 
 int update_frame() {
-    SGFFillColor(0.0f,0.0f,0.0f,1.0f);
+    SGFFillColor(1.0f,0.0f,0.0f,1.0f);
 
     draw_bg();
     SGFSetBitmapTexture(bg, 160, 144, background_texture, 0);
