8bbc1a6f1d9b3ea513f8b69f93bdfc5dec7ff111
Fonkin Stubbleduck <fonkinstubbleduck@gmail.com>
2024-11-25 17:53:34 +0100

    Removed SimpleGraphicsFramework, and am now just directly doing stuff in OpenGL (else its annoying with Raspi-GLES stuff to come)

--- makefile
+++ makefile
@@ -16,40 +16,40 @@ bin/cpu.o: src/cpu.c
 	$(CC) $(FLAGS) src/cpu.c -o bin/cpu.o
 
 bin/ppu.o: src/ppu.c
-	$(CC) $(FLAGS) -w src/ppu.c -o bin/ppu.o 
+	$(CC) $(FLAGS) src/ppu.c -o bin/ppu.o 
 
 bin/lcd.o: src/lcd.c
-	$(CC) $(FLAGS) -w src/lcd.c -o bin/lcd.o
+	$(CC) $(FLAGS) src/lcd.c -o bin/lcd.o
 
 bin/gameboy.o: src/gameboy.c
-	$(CC) $(FLAGS) -w src/gameboy.c -o bin/gameboy.o
+	$(CC) $(FLAGS) src/gameboy.c -o bin/gameboy.o
 
 bin/interrupts.o: src/interrupts.c
-	$(CC) $(FLAGS) -w src/interrupts.c -o bin/interrupts.o 
+	$(CC) $(FLAGS) src/interrupts.c -o bin/interrupts.o 
 
 bin/joypad.o: src/joypad.c
-	$(CC) $(FLAGS) -w src/joypad.c -o bin/joypad.o
+	$(CC) $(FLAGS) src/joypad.c -o bin/joypad.o
 
 bin/bus.o: src/bus.c
-	$(CC) $(FLAGS) -w src/bus.c -o bin/bus.o
+	$(CC) $(FLAGS) src/bus.c -o bin/bus.o
 
 bin/timer.o: src/timer.c
-	$(CC) $(FLAGS) -w src/timer.c -o bin/timer.o
+	$(CC) $(FLAGS) src/timer.c -o bin/timer.o
 
 bin/mbc.o: src/mbc.c
-	$(CC) $(FLAGS) -w src/mbc.c -o bin/mbc.o
+	$(CC) $(FLAGS) src/mbc.c -o bin/mbc.o
 
 bin/mbc0.o: src/mbc/mbc0.c
-	$(CC) $(FLAGS) -w src/mbc/mbc0.c -o bin/mbc0.o
+	$(CC) $(FLAGS) src/mbc/mbc0.c -o bin/mbc0.o
 
 bin/mbc1.o: src/mbc/mbc1.c
-	$(CC) $(FLAGS) -w src/mbc/mbc1.c -o bin/mbc1.o
+	$(CC) $(FLAGS) src/mbc/mbc1.c -o bin/mbc1.o
 
 bin/apu.o: src/apu.c
-	$(CC) $(FLAGS) -w src/apu.c -o bin/apu.o
+	$(CC) $(FLAGS) src/apu.c -o bin/apu.o
 
 bin/mbc3.o: src/mbc/mbc3.c
-	$(CC) $(FLAGS) -w src/mbc/mbc3.c -o bin/mbc3.o
+	$(CC) $(FLAGS) src/mbc/mbc3.c -o bin/mbc3.o
 
 clean:
 	rm -rf $(BIN)
--- src/ppu.c
+++ src/ppu.c
@@ -1,36 +1,187 @@
 #include "ppu.h"
-#include "../include/sgf.h"
+//#include "../include/sgf.h"
 #include "joypad.h"
 #include "lcd.h"
-#include <OpenGL/OpenGL.h>
 #include <stdint.h>
 #include <stdio.h>
 
-SGFwindow window;
+#define GL_SILENCE_DEPRECATION
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl3.h>
 
-uint8_t texture[160*144];
-uint8_t faux_bg_texture[160*144];
-SGFsprite bg;
+
+GLFWwindow *window;
+uint8_t screen[160*144];
+uint8_t faux_bg_screen[160*144];
+
+unsigned int shader;
+unsigned int vao;
+unsigned int texture;
 
 
-void key_callback(SGFwindow window, int key, int scancode, int action, int mods);
+const char* vertexShader = "#version 330 core\n" \
+                           "layout (location = 0) in vec3 aPos;\n" \
+                           "layout (location = 1) in vec2 aTexCoord;\n" \
+                           "uniform vec2 pos;\n" \
+                           "out vec2 texCoord;\n" \
+                           "void main(){\n" \
+                           "\tgl_Position = vec4(aPos.x + pos.x, aPos.y + pos.y, aPos.z, 1.0);\n" \
+                           "\ttexCoord = aTexCoord;\n" \
+                           "}\n";
+
+
+const char* fragmentShader = "#version 330 core\n" \
+                            "out vec4 FragColor;\n" \
+                            "in vec2 texCoord;\n" \
+                            "uniform sampler2D inTexture;\n" \
+                            "uniform vec3 colors[4];\n" \
+                            "void main() {\n" \
+                            "\tint col = int(texture(inTexture, texCoord).r*256);\n" \
+                            "\tFragColor = vec4(colors[col],1.0f);\n" \
+                            "}\n";
+
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int PPUInit(char* title) {
-    window = SGFCreateWindow(640,576, title, 0);
-    bg = SGFCreateBitmapSprite(window, 0, 0, 640, 576, 160, 144, texture, 0);
-    SGFSetKeyCallback(window, key_callback);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    window = glfwCreateWindow(640, 576, title, NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+
+    float vertices[] = {
+        1.0, -1.0, 0.0f, 1.0f, 1.0f,
+        1.0,1.0, 0.0f, 1.0f, 0.0f,
+        -1.0,1.0, 0.0f, 0.0f, 0.0f,
+        -1.0, -1.0, 0.0f, 0.0f, 1.0f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 20*sizeof(float), vertices, GL_STATIC_DRAW);
+    unsigned int ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(float), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) 0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    
+    glBindVertexArray(0);
+    
+
+    unsigned int vs;
+    vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShader, NULL);
+    glCompileShader(vs);
+    int success;
+    char error_log[512];
+    glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vs, 512, NULL, error_log);
+        printf("ERROR IN VERTEX SHADER:\n%s\n", error_log);
+    }
+
+
+    unsigned int fs;
+    fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShader, NULL);
+    glCompileShader(fs);
+    glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fs, 512, NULL, error_log);
+        printf("ERROR IN FRAGMENT SHADER:\n%s\n", error_log);
+    }
+
+    shader = glCreateProgram();
+    glAttachShader(shader, vs);
+    glAttachShader(shader, fs);
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader, 512, NULL, error_log);
+        printf("ERROR IN SHADER COMPILE:\n%s\n", error_log);
+    }
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, 160,144, 0, GL_RED, GL_UNSIGNED_BYTE, screen);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glfwSetKeyCallback(window, key_callback);
     
     float colors[4][3] = {{0.79, 0.86, 0.58}, {0.54, 0.63, 0.41}, {0.26, 0.37, 0.24}, {0.05, 0.09, 0.07}};
-    glUseProgram(bg.object.shader);
-    glUniform3fv(glGetUniformLocation(bg.object.shader, "colors"), 4, colors);
+    glUseProgram(shader);
+    glUniform3fv(glGetUniformLocation(shader, "colors"), 4, (float*)colors);
 
     glfwSwapInterval(0);
+    glBindVertexArray(vao);
     return 1;
 }
 
+int renderFrame() {
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 160, 144, GL_RED, GL_UNSIGNED_BYTE, screen);
+    
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glUseProgram(shader);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    return !glfwWindowShouldClose(window);
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if      (key == GLFW_KEY_UP) dpad_up = action == GLFW_RELEASE ? false : true;
+    else if (key == GLFW_KEY_LEFT) dpad_left = action == GLFW_RELEASE ? false : true;
+    else if (key == GLFW_KEY_DOWN) dpad_down = action == GLFW_RELEASE ? false : true;
+    else if (key == GLFW_KEY_RIGHT) dpad_right = action == GLFW_RELEASE ? false : true;
+    else if (key == GLFW_KEY_BACKSPACE) button_select = action == GLFW_RELEASE ? false : true;
+    else if (key == GLFW_KEY_ENTER) button_start = action == GLFW_RELEASE ? false : true;
+    else if (key == GLFW_KEY_Z) button_b = action == GLFW_RELEASE ? false : true;
+    else if (key == GLFW_KEY_X) button_a = action == GLFW_RELEASE ? false : true;
+    else if (key == GLFW_KEY_Q) exit(0);
+}
+
+
+
 
 void PPUKill() {
-    SGFTerminate();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
 
 // TODO Notes FOr TOmorrow
@@ -53,30 +204,11 @@ void drawScanline(int scanline, int vsync) {
             for (int tx = 0; tx < 8; tx++) {
                 uint8_t x = (tilex * 8 + tx + SCX) % 256;
                 if (x < 0 || x >= 160) continue;
-                texture[scanline*160+x] = BGP[((first >> (7-tx)) & 1) + (((second >> (7-tx)) & 1) << 1)];
+                screen[scanline*160+x] = BGP[((first >> (7-tx)) & 1) + (((second >> (7-tx)) & 1) << 1)];
             }
         }
-        /*for (int x = 0; x < 160;x++) {
-            uint8_t tx = (x + SCX) % 256;
-            uint8_t tilex = tx / 8;
-            uint8_t tiley = ty / 8;
-
-            uint8_t index = BusRead(BGTileMap + tilex + tiley*32);
-            int tile = BGWinTileData + (int)(BGWinTileData == 0x8000 ? (uint8_t)index : (int8_t)index) * 16;
-            // make tmu
-            uint8_t first = BusRead(tile + (ty%8)*2);
-            uint8_t second = BusRead(tile + (ty%8)*2+1);
-
-            uint8_t color = (((first >> (7-tx))) & 1)) + (((second >> (7-(tx%8))) & 1) << 1);
-            texture[scanline * 160 + x] = BGP[color];
-            faux_bg_texture[scanline*160+x] = color;
-
-        }*/
-
-
 
         if (WinEnable && WX >= 0 && WX <= 166 && WY >= 0 && WY <= 143) {
-            uint8_t wy = (scanline + WY) % 256;
             uint8_t tiley = y / 8;
             uint8_t ty = (scanline + WY) % 8; 
             for (int tilex = WX / 8; tilex < 21 + WX / 8; tilex++) {
@@ -87,38 +219,11 @@ void drawScanline(int scanline, int vsync) {
                 for (int tx = 0; tx < 8; tx++) {
                     uint8_t wx = tilex * 8 + tx;
                     if (wx < 0 || wx >= 160) continue;
-                    texture[scanline*160+wx] = BGP[((first >> (7-tx)) & 1) + (((second >> (7-tx)) & 1) << 1)];
+                    screen[scanline*160+wx] = BGP[((first >> (7-tx)) & 1) + (((second >> (7-tx)) & 1) << 1)];
                 }
             }
-            /*uint8_t ty = (scanline + WY);
-            for (int x = 0; x < 160;x++) {
-                uint8_t tx = (x + WX-7);
-                if (tx < 0 || tx >= 160) continue;
-                uint8_t tilex = tx / 8;
-                uint8_t tiley = ty / 8;
-
-                uint8_t index = BusRead(WinTileMap + tilex + tiley*32);
-                int tile = BGWinTileData;
-                if (BGWinTileData == 0x8000) tile += (int)((uint8_t)index) * 16;
-                if (BGWinTileData == 0x9000) tile += (int)((int8_t)index) * 16;
-                //printf("%.4X: %.4X (%.2X)\n", BGWinTileData, tile, index);
-
-                uint8_t first = BusRead(tile + (ty%8)*2);
-                uint8_t second = BusRead(tile + (ty%8)*2+1);
-
-                uint8_t color = (((first >> (7-(tx%8))) & 1)) + (((second >> (7-(tx%8))) & 1) << 1);
-                texture[(scanline * 160 + x)] = BGP[color];
-                faux_bg_texture[scanline*160+x] = color;
-            }*/
         }
-    } else {
-        /*for (int x = 0; x < 160; x++) {
-            texture[(scanline * 160 + x)*3] = colors[0][0];
-            texture[(scanline * 160 + x)*3+1] = colors[0][1];
-            texture[(scanline * 160 + x)*3+2] = colors[0][2];
-        }*/
-    }
-    
+    }     
 
     // TODO Support for Sprite Flipping
     // TODO Support For 8x16 sprites
@@ -140,12 +245,12 @@ void drawScanline(int scanline, int vsync) {
                 for (int ox = 0; ox < 8; ox++) {
                     if (ox+x < 0) continue;
                     if (ox+x >= 168) break;
-                    if (flags & 0x80 && faux_bg_texture[scanline*160+ox+x-8] > 0) continue; // BG Priority
+                    if (flags & 0x80 && faux_bg_screen[scanline*160+ox+x-8] > 0) continue; // BG Priority
                     uint8_t color;
                     if (flags & 0x20) color = (((first >> ((ox%8))) & 1)) + (((second >> ((ox%8))) & 1) << 1); // X Flip
                     else color = (((first >> (7-(ox%8))) & 1)) + (((second >> (7-(ox%8))) & 1) << 1);
                     if (color == 0) continue;
-                    texture[scanline * 160 + ox+x-8] = obp[color-1];
+                    screen[scanline * 160 + ox+x-8] = obp[color-1];
                     }
                 }
             }
@@ -153,23 +258,4 @@ void drawScanline(int scanline, int vsync) {
 }
 
 
-int renderFrame() {
-    SGFReDrawBitmapTexture(bg, 160, 144, texture, 0);
-    SGFDrawSprite(bg);
 
-    SGFLoopEnd(window);
-    return SGFRunning(window);
-}
-
-
-void key_callback(SGFwindow window, int key, int scancode, int action, int mods) {
-    if      (key == GLFW_KEY_UP) dpad_up = action == GLFW_RELEASE ? false : true;
-    else if (key == GLFW_KEY_LEFT) dpad_left = action == GLFW_RELEASE ? false : true;
-    else if (key == GLFW_KEY_DOWN) dpad_down = action == GLFW_RELEASE ? false : true;
-    else if (key == GLFW_KEY_RIGHT) dpad_right = action == GLFW_RELEASE ? false : true;
-    else if (key == GLFW_KEY_BACKSPACE) button_select = action == GLFW_RELEASE ? false : true;
-    else if (key == GLFW_KEY_ENTER) button_start = action == GLFW_RELEASE ? false : true;
-    else if (key == GLFW_KEY_Z) button_b = action == GLFW_RELEASE ? false : true;
-    else if (key == GLFW_KEY_X) button_a = action == GLFW_RELEASE ? false : true;
-    else if (key == GLFW_KEY_Q) exit(0);
-}
